9fdcd12ccc51df379824d0aa1509c0820609a278
Temurgugu <t.gugushvili@hotmail.com>
2022-05-07 20:35:23 +0400

    update

--- Entries.R
+++ Entries.R
@@ -13,7 +13,12 @@ library(hrbrthemes)
 library(waffle)
 library(magrittr)
 library(dplyr)
-
+library(ggtext)
+library(gridExtra)
+library(readxl)
+library(grid)
+library(patchwork)
+library(httr)
 
 #Import data
 
@@ -29,7 +34,7 @@ transform(Year = as.numeric(Year))
 RussiaTrips <- VisitorsTripsYear %>%
   group_by(Year) %>%
   mutate(TotalTrips = sum(Trips)) %>%
-  filter(Country == "Russia") 
+  filter(Country == "Russia" , Year > 2011) 
   
 RussiaTripsSelect <- RussiaTrips  %>% 
   group_by(Year, TotalTrips) %>%
@@ -38,7 +43,7 @@ RussiaTripsSelect <- RussiaTrips  %>%
   mutate(Percentage = round(RussiaTripsYearly/TotalTrips*100, 1)) %>%
   pivot_longer(cols = starts_with("R"), names_to = "Entries", values_to = "Number", values_drop_na = TRUE)  %>%
   arrange(desc(Year), Entries) %>%
-  add_column( x = rep(c(1, 2), 12))
+  add_column( x = rep(c(1, 2), 11))
 
 
 RussiaTripsSelect$Month_label = paste0(RussiaTripsSelect$Percentage, " %") %>% fct_inorder
@@ -50,27 +55,82 @@ TripsGeorgia <- ggplot(RussiaTripsSelect, aes(x=Entries, y=Number)) +
             aes(label = Month_label, y = Number - 10),
             hjust = 0, nudge_x = 0.05) +
   theme_void()+
-  theme(plot.background = element_rect(fill = "White", colour = "White"),
-    legend.position = "bottom", 
-    strip.text = ggtext::element_textbox(size = 11,
-    color = "white", fill = "#5D729D", box.color = "#4A618C",
+  theme(plot.title = element_markdown(size=20, colour="#035f8a", hjust=0),
+    plot.subtitle = element_markdown (size = 11, lineheight=1.3),
+    plot.caption =     element_markdown (size = 8),
+    plot.background = element_rect(fill = "White", colour = "White"),
+    legend.position = "none", 
+    strip.text = ggtext::element_textbox(size = 10,
+    color = "black", fill = "#d8dce6", box.color = "#d8dce6",
     halign = 0.5, linetype = 1, r = unit(5, "pt"), width = unit(1, "npc"),
     padding = margin(2, 0, 1, 0), margin = margin(3, 3, 3, 3)))+
-  labs(title = "The Russia",
-    caption = "Source : Information Centre, Information and Analytical Department, Ministry of Internal Affairs of Georgia | Author:Temur Gugushvili",
-       subtitle ="test",
+  labs(title = "Is every guest a gift from God?!",
+    caption = "Source: AskGov.ge|Ministry of Internal Affairs of Georgia<br>Author:Temur Gugushvili",
+       subtitle = "In the eyes of Georgians, the number of <span style = 'color: #8a0303'>Russians</span> entering the country in parallel with the<br><span style = 'color: #8a0303'>Russians</span>-Ukrainian war has surged substantially.<br><span style = 'color: #8a0303'>**True or not true?**</span>",
        x = "",
        y = "")+
   scale_fill_manual(values=c("#808080", "#8a0303"))+
   facet_wrap(vars(Year))
 
+TripsGeorgia
+
+
+tib_summary_text2 <- tibble(
+  x = -3.5, 
+  y = c(0.3), 
+  label = c("Georgia hosted 35,028<br> <span style = 'color: #8a0303'>Russians</span> in the  first 25 days<br> of the <span style = 'color: #8a0303'>Russia</span>-Ukraine war.<br> However, prior to the Covid-19<br>outbreak, 1 in every 5 visits was <br>from <span style = 'color: #8a0303'>Russia, </span> while in 2022<br>(January- April) only 14 percent<br>of the trips were from <span style = 'color: #8a0303'>Russians.</span>"))
+
+
+tib_summary_text <- tibble(
+  x = 0.9, 
+  y = c(1.6), 
+  label = c("International Visitor Trips<br>*in 2012*<br><span style = 'color: #808080'>Other Countries - **3,695,547**</span> <br>VS <br><span style = 'color: #8a0303'>Russia - **410,327**</span>"))
+
+
+# Create text plot with geom_richtext() and theme_void()
+text_plot <- tib_summary_text %>% 
+  ggplot() +
+  geom_richtext(
+    aes(x, y, label = label),
+    size = 3.5,
+    hjust = 0,
+    label.padding = unit(c(0.25, 0.25, 0.25, 0.25), "lines"),
+    label.margin = unit(c(0, 0, 0, 0), "lines"),
+    label.r = unit(0.9, "lines"),
+    label.colour = NA,
+    family = "Sylfaen"
+  ) +
+  coord_cartesian(xlim = c(0, 1), ylim = c(0, 2), clip = 'off') +
+  # clip = 'off' is important for putting it together later.
+  theme_void(base_family="Sylfaen")
+  
+
+  text_plot2 <- tib_summary_text2 %>% 
+  ggplot() +
+  geom_richtext(
+    aes(x, y, label = label),
+    size = 3.5,
+    hjust = 0,
+    label.padding = unit(c(0.25, 0.25, 0.25, 0.25), "lines"),
+    label.margin = unit(c(0, 0, 0, 0), "lines"),
+    label.r = unit(0.9, "lines"),
+    label.colour = NA,
+    family = "Sylfaen"
+  ) +
+  coord_cartesian(xlim = c(0, 1), ylim = c(0, 2), clip = 'off') +
+  # clip = 'off' is important for putting it together later.
+  theme_void(base_family="Sylfaen")
+
 
 ggsave("Visualization/TripsGeorgia.png", 
-       TripsGeorgia, 
-       width =40, 
+       grid.arrange(text_plot, TripsGeorgia, text_plot2,
+                    ncol=3, widths = c(0.2,2,0.2)),  
+       width =35, 
        height = 20, 
        units = "cm", 
        dpi = 400)
+
+
 
 
 ## რუსეთ-უკრაინის დაწყებიდან 25 დღის შემდეგ  35,028 რუსეთის მოქალაქემ შემოვიდა საქართველოში
